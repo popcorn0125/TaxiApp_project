0d53d49e1f0e53fe7c3b057756498d59bd1ac37a
김준식 <rlawnstlr54@gmail.com>
2024-09-22 15:00:45 +0900

    first commit

---
+++ src/Main.tsx
@@ -0,0 +1,44 @@
+import { SafeAreaView, StyleSheet, Text } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+import Main_Map from "./Main_Map";
+import Main_List from "./Main_List";
+import Main_Setting from "./Main_Setting";
+
+
+function Main() : JSX.Element {
+    console.log("-- Main()")
+
+    const BottomTab = createBottomTabNavigator()
+
+    return (
+        <BottomTab.Navigator>
+            <BottomTab.Screen name="Main_Map" component={Main_Map} 
+            options={{headerShown:false,
+                tabBarIcon: ({color, size}) => (<Icon name="map" size={size} color={color}/>)
+            }}/>
+            <BottomTab.Screen name="Main_List" component={Main_List} 
+            options={{headerShown:false,
+                tabBarIcon: ({color, size}) => (<Icon name="phone" size={size} color={color}/>)
+            }}/>
+            <BottomTab.Screen name="Main_Setting" component={Main_Setting} 
+            options={{headerShown:true, title:'환경설정',
+                tabBarIcon: ({color, size}) => (<Icon name="cog" size={size} color={color}/>)
+            }}/>
+        </BottomTab.Navigator>
+    )
+}
+
+const styles = StyleSheet.create({
+    textBlack: {
+        fontSize: 18,
+        color: 'black'
+    },
+    textBlue: {
+        fontSize: 18,
+        color: 'blue'
+    }
+})
+
+export default Main
---
+++ src/TaxiApp.tsx
@@ -0,0 +1,46 @@
+import { SafeAreaView, StyleSheet, Text } from "react-native";
+import { NavigationContainer} from '@react-navigation/native';
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Intro from './Intro'
+import Main from './Main'
+import Login from "./Login";
+import Register from "./Register";
+import NickNameScreen from "./Main_Setting_NickName";
+
+function TaxiApp() : JSX.Element {
+    console.log("-- TaxiApp()")
+
+    const Stack = createStackNavigator()
+
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name='Intro' component={Intro}
+                options={{headerShown: false}} />
+                <Stack.Screen name='Login' component={Login}
+                options={{headerShown: false}} />
+                <Stack.Screen name='Rigster' component={Register}
+                options={{headerShown: true, title:'회원가입'}} />
+                <Stack.Screen name='Main' component={Main}
+                options={{headerShown: false}} />
+                <Stack.Screen name='NickName' component={NickNameScreen}
+                options={{headerShown: false}} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+
+}
+
+const styles = StyleSheet.create({
+    textBlack: {
+        fontSize: 18,
+        color: 'black'
+    },
+    textBlue: {
+        fontSize: 18,
+        color: 'blue'
+    }
+})
+
+export default TaxiApp
